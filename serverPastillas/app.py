bb61b17d71e6d714670433cd72f95ef8302ee7a4
soykilian <mariavdacosta.27@gmail.com>
2022-03-11 17:09:06 +0100

    sistema de programacion de dosis implementada

--- serverPastillas/app.py
+++ serverPastillas/app.py
@@ -11,13 +11,23 @@ from datetime import datetime
 import pytz
 
 app = Flask(__name__)
+hours = ["Desayuno", "Comida", "Cena", "Noche"]
 schedules_dict = {
     "Desayuno" : "",
     "Comida" : "",
     "Cena" : "",
     "Noche" : "",
 }
-
+week_dict = {
+    "L": [],
+    "M": [],
+    "X": [],
+    "J": [],
+    "V": [],
+    "S": [],
+    "D": []
+}
+df = pandas.DataFrame(week_dict)
 @app.route("/index", methods = ["GET"])
 def index():
     return render_template("index.html")
@@ -26,17 +36,20 @@ def index():
 def time():
     if request.method == 'POST':
        body = request.data.decode()
-       print(type(body))
-       print("traza post")
        update_dict(body)
-       print(schedules_dict)
        return ("{ \"name\": \"diego\"}")
 
     elif request.method == 'GET':
-        print("traza GET_TIME1 ")
         json_sch = jsonify(schedules_dict)
-        print("traza GET_TIME 2")
         return (json_sch)
+
+@app.route("/week", methods=["POST"])
+def week():
+    body = request.data
+    print(body.decode())
+    update_df(body.decode())
+    return ("{ \"name\": \"diego\"}")
+
 
 def update_dict(body) :
    for key in schedules_dict:
@@ -45,26 +58,42 @@ def update_dict(body) :
             schedules_dict[key] = body
             break
 
+def update_df(body):
+    row = []
+    scheduler.start()
+    values = body.split(',')
+    for k in range(28):
+        row.append(values[k])
+        if (k + 1 ) % 7 == 0 :
+            print(row)
+            df.loc[len(df.index)] = row
+            row = []
+ 
+        
 def compare_time(time1, time2):
-    print(time2)
+    print("hoy toca dosis")
+    x = datetime
+    if not(time1) or not(time2):
+        return 
     time2 = time2.strip('\"')
     act_hour,act_min,sec2 = time1.split(":")
     hour,min = time2.split(":")
-    print("Hora actual", act_hour)
-    print("Hora pastilli", hour)
     if act_hour == hour :
         if int(min) - int(act_min) < 5 :
             print("Se vienen drogitas en",  int(min) - int(act_min), "minutos" )
 
 def check_schedules():
     now = datetime.now()
+    day = now.weekday()
+    print("numero dia", day)
     current_time = now.strftime("%H:%M:%S")
-    for key in schedules_dict:
-        if schedules_dict[key]:
-            compare_time(current_time, schedules_dict[key])
+    for i in range(4):
+        row = df.iloc[i]
+        print(row)
+        if row[day] == '1':
+            compare_time(current_time, schedules_dict[hours[i]])
 
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(func=check_schedules, trigger="interval", seconds=10)
-scheduler.start()            
+scheduler.add_job(func=check_schedules, trigger="interval", seconds=60)
 
