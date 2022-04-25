9c7a4ededc6350251d67981975f3d928f15e0c6e
soykilian <mariavdacosta.27@gmail.com>
2022-04-25 18:11:12 +0200

    usb

---
+++ connect.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import serial
+import time
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+    ser.reset_input_buffer()
+
+    while True:
+        ser.write(b"Hello from Raspberry Pi!\n")
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+        time.sleep(1)
